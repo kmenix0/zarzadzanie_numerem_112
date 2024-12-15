d01506045b108718e8276fb8961f467f7c1c9ace
kmenix <190845119+kmenix0@users.noreply.github.com>
2024-12-15 17:55:31 +0100

    last last

--- src/myMath.java
+++ src/myMath.java
@@ -1,6 +1,6 @@
-public class myMath {
-    public static int random(int min, int max) {
-        double randomValue = Math.random() * (max - min) + min;
-        return (int)randomValue;
+public class myMath { // Definicja klasy myMath
+    public static int random(int min, int max) { // definicja metody random, która przyjmuje dwie wartości o typie int
+        double randomValue = Math.random() * (max - min) + min; // Wbudowany sposób na losowanie liczby
+        return (int)randomValue; // Ucięcie wartości po przecinku, przez co wartość double staje sie int'em
     }
 }
